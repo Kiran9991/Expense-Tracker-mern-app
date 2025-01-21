5d1c0cae6b3a91a01a09124cdfe9cd6581757d35
Suraj <122242670+Kiran9991@users.noreply.github.com>
2025-01-21 08:55:39 +0530

    created navBar component

--- client/src/pages/Header/Header.js
+++ client/src/pages/Header/Header.js
@@ -1,24 +1,22 @@
 import { useContext } from "react";
 import { UserContext } from "../../store/user-context";
-import styles from "./Header.module.css";
-import { Link, useNavigate, useLocation, Navigate } from "react-router-dom";
-
-import symbols from "../../images/Symbol";
-
 import { expenseContext } from "../../store/expense-context";
-import { LocalHost } from "../..";
+import styles from "./Header.module.css";
+import { useNavigate, useLocation } from "react-router-dom";
+import symbols from "../../images/Symbol";
+import NavBarLink from "./NavBarLink";
 import decodeToken from "../../hook/decodeToken";
+import { LocalHost } from "../..";
 
 const Header = () => {
   const { isLogin, setIsLogin, isPremium, setIsPremium } =
     useContext(UserContext);
+  const { page, deleteAllExpense } = useContext(expenseContext);
   const navigate = useNavigate();
-  const location = useLocation();
-  const { page } = useContext(expenseContext);
   const token = localStorage.getItem("token");
   const { username } = decodeToken(token) || "";
-  const { deleteAllExpense } = useContext(expenseContext);
 
+  // Handles user sign-out
   const signoutHandler = () => {
     navigate("/sign-in");
     localStorage.clear();
@@ -27,6 +25,7 @@ const Header = () => {
     setIsPremium(false);
   };
 
+  // Handles premium purchase
   const buyPremiumBtnHandler = async () => {
     try {
       const response = await fetch(`${LocalHost}/expense/buy-premium`, {
@@ -39,61 +38,36 @@ const Header = () => {
       if (!response.ok) {
         throw new Error(data.message);
       }
-      alert(`${data.message}`);
+      alert(data.message);
       setIsPremium(true);
       localStorage.setItem("isPremium", data.response);
     } catch (error) {
-      console.log(error.message);
+      console.error("Error:", error.message);
     }
   };
 
   return (
     <nav className={styles.nav}>
       <div className={styles.navItemsLeftContainer}>
-        {isLogin && [
-          <Link to={"/expense/form"} key={Math.random()}>
-            <img
-              className={styles.expenseSymbol}
-              src={symbols.Expense}
-              alt="Expense Tracker symbol"
-            />
-          </Link>,
-          <Link
-            to={"/expense/dashboard"}
-            className={styles.navLeftItem}
-            key={Math.random()}
-          >
-            Dashboard
-          </Link>,
-          <Link
-            to={"/expense/accounts"}
-            className={styles.navLeftItem}
-            key={Math.random()}
-          >
-            Account
-          </Link>,
-          <Link
-            to={"/expense/form"}
-            className={styles.navLeftItem}
-            key={Math.random()}
-          >
-            Form
-          </Link>,
-          <Link
-            to={`/expense/expenses/${page}`}
-            className={styles.navLeftItem}
-            key={Math.random()}
-          >
-            List
-          </Link>,
-          <Link
-            to={"/expense/about-us"}
-            className={styles.navLeftItem}
-            key={Math.random()}
-          >
-            About Us
-          </Link>,
-        ]}
+        {isLogin && (
+          <>
+            {/* Expense Tracker Symbol */}
+            <NavBarLink to={"/expense/form"}>
+              <img
+                className={styles.expenseSymbol}
+                src={symbols.Expense}
+                alt="Expense Tracker symbol"
+              />
+            </NavBarLink>
+
+            {/* Navigation Links */}
+            <NavBarLink to={"/expense/dashboard"}>Dashboard</NavBarLink>
+            <NavBarLink to={"/expense/accounts"}>Account</NavBarLink>
+            <NavBarLink to={"/expense/form"}>Form</NavBarLink>
+            <NavBarLink to={`/expense/expenses/${page}`}>List</NavBarLink>
+            <NavBarLink to={"/expense/about-us"}>About Us</NavBarLink>
+          </>
+        )}
       </div>
 
       <div className={styles.navItemsRightContainer}>
@@ -116,15 +90,13 @@ const Header = () => {
         )}
 
         {isLogin && (
-          <div className="my-3 bg-purple-300 rounded-lg flex items-center w-fit px-2 border border-black p-5">
+          <div
+            className="my-3 bg-purple-300 rounded-lg flex items-center 
+            w-fit px-2 border border-black p-5"
+          >
             {username}
           </div>
         )}
-        {/* {!isLogin && (
-          <Link to={"/sign-in"} className={styles.navRightItem}>
-            <img className={styles.navBtn} src={symbols.Signin} alt="sign in" />
-          </Link>
-        )} */}
 
         {isLogin && (
           <img
---
+++ client/src/pages/Header/NavBarLink.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { NavLink, useLocation } from "react-router-dom";
+
+export default function NavBarLink({ to, children }) {
+
+  return (
+    <NavLink
+      to={to}
+      className={({ isActive }) =>
+        isActive ? 
+      "underline font-medium text-gray-950" : 
+      "font-medium text-gray-400 hover:text-gray-950"
+      }
+    >
+      {children}
+    </NavLink>
+  );
+}
